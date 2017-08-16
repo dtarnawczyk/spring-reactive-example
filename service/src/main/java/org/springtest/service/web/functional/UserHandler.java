d066490dcba58c64301121cad03376a0b5c1c93d
Dominik Tarnawczyk <dtarnawczyk@gmail.com>
2017-08-16 10:24:47 +0200

    Handler refactoring

--- service/src/main/java/org/springtest/service/web/functional/UserHandler.java
+++ service/src/main/java/org/springtest/service/web/functional/UserHandler.java
@@ -3,12 +3,12 @@ package org.springtest.service.web.functional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springtest.service.service.UsersService;
 import org.springtest.service.users.User;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
 import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
 @Component
@@ -19,20 +19,16 @@ class UserHandler {
 
     Mono<ServerResponse> getUser(ServerRequest request) {
         String userName = request.pathVariable("name");
-        return service.findByName(userName)
-                .flatMap(userMono -> ok().body(Mono.just(userMono), User.class))
+        return ok().body(service.findByName(userName), User.class)
                 .switchIfEmpty(notFound().build());
     }
 
     Mono<ServerResponse> getAllUsers(ServerRequest request){
-        return ok().body(fromPublisher(service.findAll(), User.class));
+        return ok().body(service.findAll(), User.class);
     }
 
     Mono<ServerResponse> createUser(ServerRequest request) {
-        return request.bodyToMono(User.class)
-                .flatMap(user -> service.addUser(Mono.just(user)))
-                .flatMap(createdUserName ->
-                    ServerResponse.status(HttpStatus.CREATED).body(Mono.just(createdUserName), String.class)
-                );
+        return status(HttpStatus.CREATED)
+                .body(service.addUser(request.bodyToMono(User.class)), String.class);
     }
 }
